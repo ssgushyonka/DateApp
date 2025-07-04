df433a3ff0f42709a786e3b22f6348105a1d9714
ssgushyonka <orangeee04@gmail.com>
2025-07-04 17:45:40 +0500

    edit image sizes

--- DateApp/Constants/PaywallStepConstants.swift
+++ DateApp/Constants/PaywallStepConstants.swift
@@ -1,9 +1,15 @@
-import Foundation
+import UIKit
 
 enum PaywallStepConstants {
-    static let imageHeightMult: Double = 1.2
+    static let imageHeightMult: CGFloat = Device.isiPhoneSE ? 0.65 : 1.2
     static let textLeading: CGFloat = 22
     static let textTop: CGFloat = 101
     static let textHeight: CGFloat = 58
     static let topImageSpace: CGFloat = 5
+}
+
+enum Device {
+    static var isiPhoneSE: Bool {
+        return UIScreen.main.bounds.height <= 667
+    }
 }
